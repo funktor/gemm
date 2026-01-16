572a364238a7d9852b68f91779a271ce516353ee
Abhijit Mondal <amondal@roku.com>
2026-01-16 13:55:00 +0530

    updating...

--- cublas_gemm.cu
+++ cublas_gemm.cu
@@ -142,7 +142,7 @@ void gemm_fp32_cuda_tiled(
     float Pval[COARSE_FACTOR];
     for (int r = 0; r < COARSE_FACTOR; r++) Pval[r] = 0.0f;
 
-    for (int ph = 0; ph < ceil(k/float(TILE_WIDTH)); ph++) {
+    for (int ph = 0; ph < k; ph += TILE_WIDTH) {
         if (row < m && (ph + tx) < k) Mds[ty*TILE_WIDTH+tx] = a_fp32[row*k + ph + tx];
         else Mds[ty*TILE_WIDTH+tx] = 0.0f;
 
