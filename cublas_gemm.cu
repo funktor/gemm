ded63fcbe5bf1c444fa75f595362c5406aa03021
Abhijit Mondal <amondal@roku.com>
2026-01-16 21:33:45 +0530

    updating...

--- cublas_gemm.cu
+++ cublas_gemm.cu
@@ -192,26 +192,30 @@ void gemm_fp32_cuda_tiled_2D(
     for (int r = 0; r < COARSE_FACTOR_2D*COARSE_FACTOR_2D; r++) Pval[r] = 0.0f;
 
     for (int ph = 0; ph < k; ph += TILE_WIDTH) {
-        int r = 0;
-        for (int row = row_start; row < m; row += TILE_WIDTH) {
+        for (int r = 0; r < COARSE_FACTOR_2D; r++) {
+            int row = row_start + r*TILE_WIDTH;
+
             if (row < m && ph + tx < k) Mds[ty*TILE_WIDTH+tx] = a_fp32[row*k + ph + tx];
             else Mds[ty*TILE_WIDTH+tx] = 0.0f;
 
-            for (int col = col_start; col < n; col += TILE_WIDTH) {
+            for (int c = 0; c < COARSE_FACTOR_2D; c++) {
+                int col = col_start + c*TILE_WIDTH;
+
                 if (ph + ty < k && col < n) Nds[ty*TILE_WIDTH+tx] = b_fp32[(ph + ty)*n + col];
                 else Nds[ty*TILE_WIDTH+tx] = 0.0f;
                 __syncthreads();
 
-                for (int i = 0; i < TILE_WIDTH; i++) Pval[r++] += Mds[ty*TILE_WIDTH+i]*Nds[i*TILE_WIDTH+tx];
+                for (int i = 0; i < TILE_WIDTH; i++) Pval[r*COARSE_FACTOR_2D + c] += Mds[ty*TILE_WIDTH+i]*Nds[i*TILE_WIDTH+tx];
                 __syncthreads();
             }
         }
     }
 
-    int r = 0;
-    for (int row = row_start; row < m; row += TILE_WIDTH) {
-        for (int col = col_start; col < n; col += TILE_WIDTH) {
-            if (row < m && col < n) c_fp32[row*n+col] = Pval[r++];
+    for (int r = 0; r < COARSE_FACTOR_2D; r++) {
+        int row = row_start + r*TILE_WIDTH;
+        for (int c = 0; c < COARSE_FACTOR_2D; c++) {
+            int col = col_start + c*TILE_WIDTH;
+            if (row < m && col < n) c_fp32[row*n+col] = Pval[r*COARSE_FACTOR_2D + c];
         }
     }
 }
