8e6e5c10a1b364e626e8b4a388c782c7befde0b3
Abhijit Mondal <amondal@roku.com>
2026-01-28 11:47:33 +0530

    updating...

--- swizzle.py
+++ swizzle.py
@@ -1,4 +1,4 @@
-def swizzle(n, m, k):
+def swizzle(n, m, k, u):
     for i in range(n):
         h = " ".join([str(x) for x in range(m)])
         print(h)
@@ -8,11 +8,18 @@ def swizzle(n, m, k):
     for i in range(n):
         g = []
         for j in range(m):
-            g += [(int(j/k))*k + (i%k)^(j%k)]
+            g += [int(j/k)*k + (u*((i%k)^int((j%k)/u)) + ((j%k)%u)) % k]
 
         h = " ".join([str(x) for x in g])
         print(h)
 
 
 
-swizzle(32, 32, 16)
+swizzle(32, 32, 8, 2)
+
+# 0 -> u*(r ^ 0)
+# 1 -> u*(r ^ 0) + 1
+# 2 -> r ^ 1
+# 3 -> r ^ 1 + 1
+
+# j -> r ^ (j/2) + (j%2)
