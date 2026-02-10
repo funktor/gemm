0138d36c21e5bcf16fa924afca69c1f7d95e3d8b
Abhijit Mondal <amondal@roku.com>
2026-02-10 17:19:27 +0530

    updating...

--- cublas_gemm.cu
+++ cublas_gemm.cu
@@ -280,10 +280,8 @@ void gemm_fp32_cuda_tiled_2D_async(
                 __syncthreads();
 
                 pipeline.producer_acquire();
-                if (curr < COARSE_FACTOR_2D) {
-                    int ub_col = bx*TILE_WIDTH*COARSE_FACTOR_2D + curr*TILE_WIDTH;
-                    cuda::memcpy_async(Nds[stage] + ty*TILE_WIDTH, b_fp32 + (ph + ty)*n + ub_col, sizeof(float) * TILE_WIDTH, pipeline);
-                }
+                int ub_col = bx*TILE_WIDTH*COARSE_FACTOR_2D + (curr % COARSE_FACTOR_2D)*TILE_WIDTH;
+                cuda::memcpy_async(Nds[stage] + ty*TILE_WIDTH, b_fp32 + (ph + ty)*n + ub_col, sizeof(float) * TILE_WIDTH, pipeline);
                 pipeline.producer_commit();
 
                 stage = (stage + 1) % NUM_STAGES_ASYNC_PIPELINE;
