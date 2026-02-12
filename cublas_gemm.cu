4c0168e9f2566b602f05c2f05a3b7301b16cd7fb
Abhijit Mondal <amondal@roku.com>
2026-02-12 14:39:39 +0530

    updating...

--- cublas_gemm.cu
+++ cublas_gemm.cu
@@ -358,7 +358,6 @@ void gemm_fp32_cuda_tiled_2D_async_warp_spl(
                         res[2] += Mds[stage][(ty-4)*TILE_WIDTH+i]*Nds[stage][i*TILE_WIDTH+tx*4+2];
                         res[3] += Mds[stage][(ty-4)*TILE_WIDTH+i]*Nds[stage][i*TILE_WIDTH+tx*4+3];
                     }
-                    __syncthreads();
                     pipe.consumer_release();
                     s += 1;
                 }
