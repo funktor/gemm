a9469ad3cd9f2c95f1a5459b9ef1c9469043c9ac
Abhijit Mondal <amondal@roku.com>
2026-02-14 10:07:50 +0530

    updating...

--- cublas_gemm.cu
+++ cublas_gemm.cu
@@ -1270,9 +1270,9 @@ void convertFp32ToFp16 (half *out, const float *in, const long n) {
 }
 
 int main(){
-    int m = 4096;
-    int n = 4096;
-    int k = 4096;
+    int m = 2048;
+    int n = 2048;
+    int k = 1024;
 
     float *a_fp32;
     float *b_fp32;
